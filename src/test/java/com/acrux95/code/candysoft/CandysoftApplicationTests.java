c45799d1145ee1abae58cdff0db79d9b219b897c
Armando Cruz <acrux95.code@gmail.com>
2025-11-09 16:02:27 -0600

    Create Main Container Project

---
+++ src/test/java/com/acrux95/code/candysoft/CandysoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrux95.code.candysoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandysoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
