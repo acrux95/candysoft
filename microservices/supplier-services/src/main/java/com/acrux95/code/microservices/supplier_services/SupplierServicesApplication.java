cd7a41a4c3a7c2eda328ddbfe80624702f5bee14
Armando Cruz <acrux95.code@gmail.com>
2025-11-12 13:06:32 -0600

    Fixing project

---
+++ microservices/supplier-services/src/main/java/com/acrux95/code/microservices/supplier_services/SupplierServicesApplication.java
@@ -0,0 +1,13 @@
+package com.acrux95.code.microservices.supplier_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierServicesApplication.class, args);
+	}
+
+}
---
+++ microservices/supplier-services/src/test/java/com/acrux95/code/microservices/supplier_services/SupplierServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrux95.code.microservices.supplier_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
