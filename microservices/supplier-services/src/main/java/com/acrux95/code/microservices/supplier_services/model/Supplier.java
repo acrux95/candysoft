e8154cced63df1b6c984773f9225f62a058aade0
Armando Cruz <acrux95.code@gmail.com>
2025-11-12 16:39:05 -0600

    Building Supplier Services

---
+++ microservices/supplier-services/src/main/java/com/acrux95/code/microservices/supplier_services/model/Supplier.java
@@ -0,0 +1,34 @@
+package com.acrux95.code.microservices.supplier_services.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "suppliers")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "supplier_seq")
+    @SequenceGenerator(name = "supplier_seq", sequenceName = "supplier_sequence", allocationSize = 1)
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+
+    @Column(updatable = false, name = "date_created", columnDefinition = "datetime")
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    @Column(name = "date_updated", columnDefinition = "datetime")
+    private LocalDateTime dateUpdated;
+}
